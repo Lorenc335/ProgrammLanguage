fc6f415e9f106a1861a1e283e629dd7d6e0118e1
Tatiana <k.t.s-92@yandex.ru>
2022-08-12 12:57:54 +0700

    programm username

--- hellow/Program.cs
+++ hellow/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!Mama mia ");
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
