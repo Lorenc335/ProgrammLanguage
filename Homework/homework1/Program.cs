091f3e85401ccc843267b4dde602b599112cd2fc
Tatiana <k.t.s-92@yandex.ru>
2022-08-14 11:23:09 +0700

    commit for all

---
+++ Homework/homework1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b:");
+int b = Convert.ToInt32(Console.ReadLine());
---
+++ programm_UserPremium/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("wow, it is Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
